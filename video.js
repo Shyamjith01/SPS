21d256e00e246ea092075711742d8e5fd9ec31b4
jithujithutan@gmail.com <jithujithutan@gmail.com>
2021-12-15 19:35:06 +0530

    first commit

--- video.js
+++ video.js
@@ -1,14 +1,1 @@
-const spsimg = document.querySelector(".spsimg");
-const spsvideo = document.querySelector(".spsvideo");
 
-const contentplacer = () =>{
-    spsimg.classList.add("hidden");
-    spsvideo.classList.remove("hidden");
-
-    spsvideo.setAttribute(
-        "src",
-        "https://www.youtube.com/watch?v=I9QGnNvrmoY?autoplay=1"
-    );
-}
-
-spsimg.addEventListener("click",contentplacer);
